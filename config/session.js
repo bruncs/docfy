653f40b42b7391a768f784a007bcc51e4f9e68be
Bruno Correia <brunocs@msn.com>
2018-05-18 00:13:43 -0300

    Storing session on DB done.

--- config/session.js
+++ config/session.js
@@ -1,5 +1,10 @@
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('../app/models');
+
 module.exports = {
   secret: 'docfy2018bcs',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
+  store: new SequelizeStore({ db: sequelize }),
 };
