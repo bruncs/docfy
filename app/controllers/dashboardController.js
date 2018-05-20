93de9cf2debfcf6872b73d25d5157114ad512c05
Bruno Correia <brunocs@msn.com>
2018-05-19 21:04:38 -0300

    Project list on dashboard done.

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,5 +1,18 @@
+const { Project } = require('../models');
+
 module.exports = {
-  index(req, res) {
-    return res.render('dashboard/index');
+  async index(req, res, next) {
+    try {
+      const projects = await Project.findAll({
+        where: { UserId: req.session.user.id },
+      });
+
+      return res.render('dashboard/index', {
+        user: req.session.user,
+        projects,
+      });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
